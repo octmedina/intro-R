f646829bc14b5104d1bdae2003173719211abde9
Octavio Medina <omedina@ideas42.org>
2021-11-29 22:50:34 +0100

    edit

--- sesion-4/sesion-4.R
+++ sesion-4/sesion-4.R
@@ -4,38 +4,80 @@ install.packages("mapSpain", dependencies = TRUE)
 
 library(mapSpain)
 library(tidyverse)
-#library(palmerpenguins)
-library(hrbrthemes)
 library(viridis)
 
 
+# Si teneis un paquete muy grande y solo quereis una funcion
+# podeis usar el nombre del paquete y ::
+# tidyverse::select
 
-census <- mapSpain::pobmun19
+census <- pobmun19
+
 
 # Extract CCAA from base dataset
 
-codelist <- mapSpain::esp_codelist
+codelist <- esp_codelist
 
+# Merge es join
+# left_join (1), right_join (2), full_join (todo), inner_join (3)
 
-census <- census %>%
-  left_join(select(codelist, cpro, ccaa.shortname.es), by = c("cpro"))
+t <- data.frame(name = c("Maria", "Elena", "Pere"))
+t2 <- data.frame(name = c("Pere", "Cristina"))
+
+t %>%
+  left_join(t2)
+
+t %>%
+  inner_join(t2)
+
+t2 %>%
+  left_join(t)
+
+t %>%
+  full_join(t2)
+
+t %>%
+  anti_join(t2)
+
+### MAPAS
+
+# duplicates drop, force etc etc
+code_redux <- codelist %>%
+  select(cpro, ccaa.shortname.es) %>%
+  distinct(cpro, ccaa.shortname.es)
+
+census2 <- census %>%
+  left_join(code_redux, by = "cpro")
+
+census_rightjoin <- census %>%
+  left_join(code_redux, by = "cpro")
 
 
 # Summarize by CCAA
-census_ccaa <- census %>%
+census_ccaa <- census2 %>%
   rename(ccaa = ccaa.shortname.es) %>%
   group_by(ccaa) %>%
   summarize(total_men = sum(men),
             total_women = sum(women),
             total = sum(pob19)) %>%
   mutate(share_women = round(100*total_women / total, 2))
+  #mutate(share_women = 100*total_women / total)
+
 
 # Merge into spatial data
 
 CCAA_sf <- esp_get_ccaa() %>%
   rename(ccaa = ccaa.shortname.es)
+
 CCAA_sf <- left_join(CCAA_sf, census_ccaa)
+
+
 can <- esp_get_can_box()
+
+
+# geom_sf es el geom para mapas
+ggplot(CCAA_sf) +
+  geom_sf(aes(fill = share_women))
 
 
 ##
@@ -62,11 +104,31 @@ ggplot(CCAA_sf) +
   theme(legend.position = c(0.1, 0.6))
 
 
+##
+
+ggplot(CCAA_sf) +
+  geom_sf(aes(fill = share_women),
+          color = "grey70",
+          lwd = .3
+  ) +
+  geom_sf(data = can, color = "grey70") +
+  geom_sf_label(aes(label = share_women),
+                fill = "white", alpha = 0.5,
+                size = 3,
+                label.size = 0
+  ) +
+  scale_fill_viridis_c(option = "viridis", begin = 0.1, end = 0.9) +
+  theme_void() +
+  theme(legend.position = c(0.1, 0.6)) +
+  labs(title = "Esto es el % de mujeres por CCAA")
+
+
+
 # PROVINCIAS
 
 
 # Summarize by CCAA
-census_prov <- census %>%
+census_prov <- census2 %>%
   group_by(cpro) %>%
   summarize(total_men = sum(men),
             total_women = sum(women),
